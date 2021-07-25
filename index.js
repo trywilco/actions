617a567baaa8e01ba9c4bc5eaca2edac2fff2791
Shem Magnezi <smagnezi8@gmail.com>
2021-07-25 18:41:06 +0300

    test

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fetch = require("node-fetch");
+
+const test = async () => {
+  const res = await fetch("https://46edd159844c.ngrok.io/prs/check");
+  const body = await res.text();
+
+  console.log({ body });
+};
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  // const nameToGreet = core.getInput("who-to-greet");
+  // console.log(`Hello ${nameToGreet}!`);
+  test();
+  // const time = new Date().toTimeString();
+  // core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const number = github.context.issue.number;
+  console.log({ number });
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+  // core.setFailed(error.message);
 }
