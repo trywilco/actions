f4bc89e71622f75305e8dc6a37daa36d3f9daad2
Shem Magnezi <smagnezi8@gmail.com>
2021-08-03 16:41:01 +0300

    no message

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require("@actions/core");
+const exec = require("@actions/exec");
 const github = require("@actions/github");
+
 const fetch = require("node-fetch");
 
 const test = async () => {
@@ -7,6 +9,8 @@ const test = async () => {
   const body = await res.text();
 
   console.log({ body });
+
+  await exec.exec("node index.js");
 };
 
 try {
@@ -14,11 +18,12 @@ try {
   // `who-to-greet` input defined in action metadata file
   // const nameToGreet = core.getInput("who-to-greet");
   // console.log(`Hello ${nameToGreet}!`);
-  test();
+  // test();
   // const time = new Date().toTimeString();
   // core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const number = github.context.issue.number;
+  console.log({ context: github.context });
+  const number = github.context.pull.number;
   console.log({ number });
   // const payload = JSON.stringify(github.context.payload, undefined, 2);
   // console.log(`The event payload: ${payload}`);
