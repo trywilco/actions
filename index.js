6752cc126ac8b219ba5e40667f432cf5bde626a2
Shem Magnezi <smagnezi8@gmail.com>
2021-08-08 19:34:04 +0300

    try to fix try/catch

--- index.js
+++ index.js
@@ -3,28 +3,26 @@ const exec = require("@actions/exec");
 const github = require("@actions/github");
 
 const fetch = require("node-fetch");
-const { promises: fs } = require("fs");
 
 const runActions = async () => {
   const wilcoId = core.getInput("wilcoId", { required: true });
-  // const wilcoId = await fs.readFile(".wilco", "utf8");
   const res = await fetch(
     `https://46edd159844c.ngrok.io/prs/${wilcoId}/actions`
   );
   const body = await res.json();
   for (let item of body) {
-    const { cmd, ...args } = item;
-    await exec.exec(cmd, null, args);
+    try {
+      const { cmd, ...args } = item;
+      await exec.exec(cmd, null, args);
+    } catch (error) {
+      core.error("Wilco checks failed");
+      core.setFailed(error.message);
+    }
   }
 };
 
-const main = async () => {
-  try {
-    await runActions();
-  } catch (error) {
-    core.error("Wilco checks failed");
-    core.setFailed(error.message);
-  }
-};
-
-main();
+try {
+  runActions();
+} catch (error) {
+  core.setFailed(error.message);
+}
