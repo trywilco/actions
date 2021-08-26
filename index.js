b54e348230d63b93e66a592a21a0f7a2f1c6c2ed
Shem Magnezi <smagnezi8@gmail.com>
2021-08-26 19:24:01 +0300

    debug printing

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
-const github = require("@actions/github");
 const { promises: fs } = require("fs");
 
 const fetch = require("node-fetch");
@@ -13,13 +12,17 @@ const runActions = async () => {
   const body = await res.json();
   for (let item of body) {
     const { cmd, ...args } = item;
+    core.debug(`Starting: ${{ cmd, args }}`);
     await exec.exec(cmd, null, args);
+    core.debug(`Done: ${{ cmd, args }}`);
   }
 };
 
 const main = async () => {
   try {
+    core.debug("Starting all");
     await runActions();
+    core.debug("Done all");
   } catch (error) {
     core.error("Wilco checks failed");
     core.setFailed(error.message);
