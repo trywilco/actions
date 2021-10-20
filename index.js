7a6b4eaff6df41c930955395f778c74443fcb229
Shem <smagnezi8@gmail.com>
2021-10-20 12:07:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fetch = require("node-fetch");
 const runActions = async () => {
   const wilcoId = await fs.readFile(".wilco", "utf8");
   const res = await fetch(
-    `https://253e98fd5e72.ngrok.io/prs/${wilcoId}/actions`
+    `https://d195-46-117-183-63.ngrok.io/prs/${wilcoId}/actions`
   );
   const body = await res.json();
   for (let item of body) {
