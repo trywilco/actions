42265d862061dc8b1b0aaac819760551ab784832
Shem Magnezi <smagnezi8@gmail.com>
2021-11-28 16:00:24 +0200

    check for staging, so we'll use production if the argument is empty

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const exec = require("@actions/exec");
 const { promises: fs } = require("fs");
 
 const host =
-  core.getInput("appEnv") === "production"
-    ? "https://wilco-engine.herokuapp.com"
-    : "https://wilco-engine-staging.herokuapp.com";
+  core.getInput("appEnv") === "staging"
+    ? "https://wilco-engine-staging.herokuapp.com"
+    : "https://wilco-engine.herokuapp.com";
 
 const fetch = require("node-fetch");
 
