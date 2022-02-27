5e88f860849073bee249cc4d14e56d9338f21623
Erik Sapir <eriksapir@Eriks-MacBook-Pro.local>
2022-02-28 00:03:09 +0200

    update eriks ngrok url

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { promises: fs } = require("fs");
 
 const host =
   core.getInput("owner") === "Staging-ObelusFamily"
-    ? "https://2acd-79-179-169-139.ngrok.io"
+    ? "https://635e-79-179-169-139.ngrok.io"
     : "https://wilco-engine.herokuapp.com";
 
 const fetch = require("node-fetch");
