99687ab0754470f2c3d75cc0086503baa10dc932
Erik Sapir <eriksapir@Eriks-MacBook-Pro.local>
2022-03-08 18:38:18 +0200

    update ngrok urls

--- dist/index.js
+++ dist/index.js
@@ -3568,7 +3568,7 @@ const { promises: fs } = __nccwpck_require__(747);
 
 const host =
   core.getInput("owner") === "Staging-ObelusFamily"
-    ? "https://635e-79-179-169-139.ngrok.io"
+    ? "https://2263-79-179-169-139.ngrok.io"
     : "https://wilco-engine.herokuapp.com";
 
 const fetch = __nccwpck_require__(467);
