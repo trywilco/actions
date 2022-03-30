6d63d546fa666864a70cac5dc69c1db8a3e3e65a
NivRichter <nivrichter@gmail.com>
2022-03-30 16:43:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { promises: fs } = require("fs");
 
 const host =
   core.getInput("owner") === "Staging-ObelusFamily"
-    ? "https://wilco-engine-staging.herokuapp.com"
+    ? "https://37347eac0d70.ngrok.io"
     : "https://wilco-engine.herokuapp.com";
 
 const fetch = require("node-fetch");
